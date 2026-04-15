a06b7ccde229deac6bc897037904e310058a9b01
shubhangickore <koreshubhangi291@gmail.com>
2026-04-15 23:22:40 +0530

    checks if the given no is prime or not

---
+++ Prime.java
@@ -0,0 +1,15 @@
+public class Prime {
+    public static void main(String args[]) {
+        int n = 17;
+        boolean isprime = true;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                isprime = false;
+                break;
+            }
+
+        }
+        System.out.println(isprime ? " prime" : "not prime");
+    }
+
+}
